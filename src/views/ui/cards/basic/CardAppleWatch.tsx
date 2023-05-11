939e8e18ebb314169fe8e38e9bfea61b9ad3ea4f
dmr-cdda <mizan@cdda.io>
2023-05-11 09:56:35 +0600

    apple watch card button group divider removed with custom theme

--- src/views/ui/cards/basic/CardAppleWatch.tsx
+++ src/views/ui/cards/basic/CardAppleWatch.tsx
@@ -4,41 +4,48 @@ import Button from '@mui/material/Button'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
-import { Grid, Stack } from '@mui/material'
+import { ButtonGroup, ThemeProvider } from '@mui/material'
+
+import { createTheme } from '@mui/material/styles'
+
+let theme = createTheme({
+  components: {
+    MuiToggleButtonGroup: {
+      styleOverrides: {
+        grouped: {
+          '&:not(:last-of-type)': {
+            border: 'none'
+          }
+        }
+      }
+    }
+  }
+})
 
 const CardAppleWatch = () => {
   return (
-    <Card>
-      <CardMedia sx={{ height: '9.375rem' }} image='/images/cards/watch-on-hand.jpg' />
-      <CardContent sx={{ p: theme => `${theme.spacing(3, 5.25, 4)} !important` }}>
-        <Typography variant='h5' sx={{ mb: 2 }}>
-          Apple Watch
-        </Typography>
-        <Typography sx={{ mb: 2 }}>$249.40</Typography>
-        <Typography sx={{ color: 'text.secondary' }}>
-          3.1GHz 6-core 10th-generation Intel Core i5 processor, Turbo Boost up to 4.5GHz
-        </Typography>
-      </CardContent>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Button
-            variant='contained'
-            sx={{ py: 2.5, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, width: '100%' }}
-          >
-            Add To Cart
-          </Button>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Button
-            variant='contained'
-            color='error'
-            sx={{ py: 2.5, borderTopLeftRadius: 0, borderTopRightRadius: 0, width: '100%' }}
-          >
-            Purchase
-          </Button>
-        </Grid>
-      </Grid>
-    </Card>
+    <ThemeProvider theme={theme}>
+      <Card>
+        <CardMedia sx={{ height: '9.375rem' }} image='/images/cards/watch-on-hand.jpg' />
+        <CardContent sx={{ p: theme => `${theme.spacing(3, 5.25, 4)} !important` }}>
+          <Typography variant='h5' sx={{ mb: 2 }}>
+            Apple Watch
+          </Typography>
+          <Typography sx={{ mb: 2 }}>$249.40</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            3.1GHz 6-core 10th-generation Intel Core i5 processor, Turbo Boost up to 4.5GHz
+          </Typography>
+        </CardContent>
+        <ButtonGroup
+          variant='contained'
+          fullWidth
+          sx={{ borderTopLeftRadius: 0, borderBottomRightRadius: 0, border: 'none' }}
+        >
+          <Button color='error'>Buy Now</Button>
+          <Button color='success'>Add to cart</Button>
+        </ButtonGroup>
+      </Card>
+    </ThemeProvider>
   )
 }
 
